4e07e8085372f5716d2034771dc70ecaf958b12c
Eliwolf92 <elias.mainokaid@gmail.com>
2025-12-03 09:04:17 -0500

    calculette

---
+++ calculateArea/calculate_Area.js
@@ -0,0 +1,11 @@
+let length;
+let width;
+
+function calculateArea(){
+    length = parseFloat(document.getElementById('length').value);
+    width = parseFloat(document.getElementById('width').value);
+
+    let area = length * width;
+}
+
+document.getElementById('result').innerText = `le mètres carré de ce réctangle est de : ${area}`;
