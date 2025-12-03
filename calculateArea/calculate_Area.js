204b9d7979ece6e0791bdc77a2347cf748df511e
Eliwolf92 <elias.mainokaid@gmail.com>
2025-12-03 09:34:42 -0500

    caisse

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -8,3 +8,16 @@ function calculRectange() {
     let area = length * width;
     document.getElementById('result').innerText = `le mètres carré de ce réctangle est de : ${area}`;
 }
+
+let prix1;
+let prix2;
+let prix3;
+
+function calculCaisse(){
+    prix1 = parseFloat(document.getElementById('prix1').value);
+    prix2 = parseFloat(document.getElementById('prix2').value);
+    prix3 = parseFloat(document.getElementById('prix3').value);
+
+    let total = prix1 + prix2 + prix3;
+    document.getElementById('resultat').innerText = `le prix total est de : ${total}`
+}
